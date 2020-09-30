bf2d48cd9e8d47231b8543b13963bd8b251fc4ab
Andrii Guselietov <guseletov@gmail.com>
2020-09-30 17:02:09 +0200

    All changes after test

--- instances/main.tf
+++ instances/main.tf
@@ -67,7 +67,7 @@ resource "aws_security_group" "sg_22_80" {
 }
 
 resource "aws_instance" "web" {
-  ami                         = "ami-YOUR-AMI-ID"
+  ami                         = "ami-0371f3c0dab11ff43"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_public.id
   vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
